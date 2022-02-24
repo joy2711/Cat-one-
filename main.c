18c9202a0466087ea41d1fbb49a25de0918c37c7
Joy Anne Wairimu <60652642+joy2711@users.noreply.github.com>
2022-02-24 15:14:54 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 int main()
 {
-    // declare local variables
     printf("0\n");
     char opt;
     int n1, n2;
     float res;
-    printf (" Select an operator (+, -, *, /) to perform an operation in C calculator \n ");
-    scanf ("%c", &opt); // take an operator
+    printf (" Select an operator (+, -, *, /)\n ");
+    scanf ("%c", &opt);
     printf (" Enter the first number: ");
-    scanf(" %d", &n1); // take fist number
+    scanf(" %d", &n1); 
     printf (" Enter the second number: ");
-    scanf (" %d", &n2); // take second number
+    scanf (" %d", &n2); 
 
     if (opt == '+')
     {
@@ -21,13 +20,13 @@ int main()
 
     else if (opt == '-')
     {
-        res = n1 - n2; // subtract two numbers
+        res = n1 - n2; 
         printf ("  Result of %d %c %d is: %f ", n1,opt, n2, res);
     }
 
     else if (opt == '*')
     {
-        res = n1 * n2; // multiply two numbers
+        res = n1 * n2; 
         printf ("  Result of %d %c %d is: %f ", n1, opt, n2, res);
     }
 
@@ -38,7 +37,7 @@ int main()
             printf (" \n Divisor cannot be zero. Please enter another value ");
             scanf ("%d", &n2);
         }
-        res = n1 / n2; // divide two numbers
+        res = n1 / n2; 
         printf ("  Result of %d %c %d is: %.2f", n1,opt, n2, res);
     }
     else
